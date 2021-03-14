193abc50dfc06b6ccd46f9d52ac463be7b2f0232
Ani Manukyan <animanukyan95@gmail.com>
2021-03-15 00:47:59 +0400

    create symbols image

---
+++ src/Homework1.java
@@ -0,0 +1,17 @@
+public class Homework1
+{
+    public static void main(String[] args) {
+
+        String symbols = "\t\t\t\t-\n"+
+                "\t\t\t.\t\t.\n" +
+                "\t\t|\t\t|\t\t|\n" +
+                "\t-\t\t-\t\t-\t\t-\n" +
+                "-\t\tA\t\tn\t\ti\t\t-\n" +
+                "\t-\t\t-\t\t-\t\t-\n" +
+                "\t\t|\t\t|\t\t|\n" +
+                "\t\t\t.\t\t.\n" +
+                "\t\t\t\t-\n";
+
+        System.out.println(symbols);
+    }
+}
