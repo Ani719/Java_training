ebf693c5651cae1911528eb9497fe7df21869568
Ani Manukyan <animanukyan95@gmail.com>
2021-03-15 20:48:33 +0400

    Lesson

---
+++ src/Create_array.java
@@ -0,0 +1,42 @@
+
+public class Create_array {
+    public static void main(String[] args) {
+
+
+        int[] numbers = {6, 9, 21, 12, 6};
+        String[] names = {"Gay", "Sona", "gfds", "fghg", "ar"};
+
+        for(int i = 0; i < 5; i++){
+            if (numbers[i] % 2 == 0){
+                System.out.println(numbers[i] + "  " + names[i]);
+            }
+
+
+       // int num = 0;
+      //  while (num < 5){
+
+            //if (numbers[num] % 2 == 0)
+           // System.out.println(numbers[num] + " " + names[num]);
+            //num++;
+
+        }
+
+//        (numbers[0] % 2 == 0) {
+//            System.out.println(numbers[0] + " " + names[0]);
+//        }
+//        if (numbers[1] % 2 == 0) {
+//            System.out.println(numbers[1] + " " + names[1]);
+//        }
+//        if (numbers[2] % 2 == 0){
+//            System.out.println(numbers[2] + " " + names[2]);
+//        }
+//        if (numbers[3] % 3 == 0){
+//            System.out.println(numbers[3] + " " + names[3]);
+//        }
+//        if (numbers[4] % 4 == 0){
+//            System.out.println(numbers[4] + " " + names[4]);
+//        }
+    }
+
+
+}
